b138f3f9b005f6dd2a8e1c28b62a393fdd477818
Arcao <arcao@arcao.com>
2015-04-11 16:21:53 +0200

    Added OkHttp library for downloading Image data (solve HTTP redirect error with Picasso)

--- app/src/main/java/com/arcao/trackables/data/DataModule.java
+++ app/src/main/java/com/arcao/trackables/data/DataModule.java
@@ -1,6 +1,8 @@
 package com.arcao.trackables.data;
 
 import com.arcao.trackables.App;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 import dagger.Module;
 import dagger.Provides;
@@ -8,8 +10,18 @@ import dagger.Provides;
 @Module
 public final class DataModule {
 	@Provides
-	public Picasso providePicasso(App app) {
-		return Picasso.with(app);
+	public Picasso providePicasso(App app, OkHttpDownloader okHttpDownloader) {
+		return new Picasso.Builder(app).downloader(okHttpDownloader).build();
+	}
+
+	@Provides
+	public OkHttpDownloader provideOkHttpDownloader(OkHttpClient okHttpClient) {
+		return new OkHttpDownloader(okHttpClient);
+	}
+
+	@Provides
+	public OkHttpClient provideOkHttpClient() {
+		return new OkHttpClient();
 	}
 
 }
