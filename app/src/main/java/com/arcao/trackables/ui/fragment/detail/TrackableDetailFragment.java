ba1f0bef0755ebf819c8f2de1bac64789a02e93e
Arcao <arcao@arcao.com>
2015-05-27 09:16:11 +0200

    Initial work on TrackableDetailFragment layout

--- app/src/main/java/com/arcao/trackables/ui/fragment/detail/TrackableDetailFragment.java
+++ app/src/main/java/com/arcao/trackables/ui/fragment/detail/TrackableDetailFragment.java
@@ -2,6 +2,8 @@ package com.arcao.trackables.ui.fragment.detail;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.arcao.geocaching.api.data.Trackable;
 import com.arcao.trackables.R;
@@ -9,19 +11,36 @@ import com.arcao.trackables.data.service.DataSource;
 import com.arcao.trackables.data.service.TrackableService;
 import com.arcao.trackables.exception.ExceptionHandler;
 import com.arcao.trackables.internal.rx.AndroidSchedulerTransformer;
+import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import rx.android.app.AppObservable;
-import rx.internal.util.SubscriptionList;
 
 public class TrackableDetailFragment extends AbstractTrackableFragment {
 	@Inject
 	protected TrackableService trackableService;
 	@Inject
 	protected ExceptionHandler exceptionHandler;
+	@Inject
+	protected Picasso picasso;
 
-	private final SubscriptionList subscriptionList = new SubscriptionList();
+	@InjectView(R.id.image)
+	protected ImageView imageView;
+
+	@InjectView(R.id.title)
+	protected TextView titleView;
+
+	@InjectView(R.id.tracking_code)
+	protected TextView trackingCodeView;
+
+	@InjectView(R.id.description)
+	protected TextView descriptionView;
+
+	@InjectView(R.id.goal)
+	protected TextView goalView;
 
 	public static TrackableDetailFragment newInstance(String trackableCode) {
 		TrackableDetailFragment fragment = new TrackableDetailFragment();
@@ -44,6 +63,8 @@ public class TrackableDetailFragment extends AbstractTrackableFragment {
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
+		ButterKnife.inject(this, view);
+
 		AppObservable.bindFragment(this, trackableService.getTrackable(getTrackableCode(), DataSource.REMOTE_IF_NECESSARY))
 						.compose(AndroidSchedulerTransformer.get())
 						.subscribe(this::showTrackableDetail, throwable -> {
@@ -53,6 +74,17 @@ public class TrackableDetailFragment extends AbstractTrackableFragment {
 	}
 
 	public void showTrackableDetail(Trackable trackable) {
+		getActivity().setTitle(trackable.getName());
 
+		titleView.setText(trackable.getName());
+		trackingCodeView.setText(trackable.getTrackingNumber());
+		descriptionView.setText(trackable.getDescription());
+		goalView.setText(trackable.getGoal());
+
+		if (trackable.getImages().size() > 0) {
+			picasso.load(trackable.getImages().get(0).getUrl()).into(imageView);
+		} else {
+			imageView.setVisibility(View.GONE);
+		}
 	}
 }
