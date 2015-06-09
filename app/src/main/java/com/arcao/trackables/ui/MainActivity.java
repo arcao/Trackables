fdbfb095677a3adc7e987b3a81bab48ed5ed797e
Arcao <arcao@arcao.com>
2015-06-09 07:18:57 +0200

    Updated to latest Material Drawer library

--- app/src/main/java/com/arcao/trackables/ui/MainActivity.java
+++ app/src/main/java/com/arcao/trackables/ui/MainActivity.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import butterknife.ButterKnife;
-import butterknife.InjectView;
+
 import com.arcao.geocaching.api.data.type.MemberType;
 import com.arcao.trackables.R;
 import com.arcao.trackables.data.service.AccountService;
@@ -15,23 +14,36 @@ import com.arcao.trackables.internal.di.component.MainActivityComponent;
 import com.arcao.trackables.ui.fragment.TrackableListFragment;
 import com.mikepenz.iconics.typeface.FontAwesome;
 import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
-import com.mikepenz.materialdrawer.model.*;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 import com.mikepenz.materialdrawer.util.KeyboardUtil;
-import timber.log.Timber;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity implements HasComponent<MainActivityComponent> {
+	private static final int ID_MY = 0;
+	private static final int ID_FAVORITE = 1;
+	private static final int ID_SETTINGS = -1;
+
 	@Inject
 	protected AccountService accountService;
 
 	@InjectView(R.id.toolbar)
 	protected Toolbar toolbar;
 
-	private AccountHeader.Result headerResult = null;
-	private Drawer.Result result = null;
+	private AccountHeader accountHeader = null;
+	private Drawer drawer = null;
 
 	private MainActivityComponent component;
 	public MainActivityComponent component() {
@@ -51,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Main
 		setSupportActionBar(toolbar);
 
 		// Create the AccountHeader
-		headerResult = new AccountHeader()
+		accountHeader = new AccountHeaderBuilder()
 						.withActivity(this)
 						.withHeaderBackground(R.drawable.header)
 						.withSelectionListEnabledForSingleProfile(false)
@@ -67,21 +79,15 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Main
 
 
 		//Create the drawer
-		result = new Drawer()
+		drawer = new DrawerBuilder()
 						.withActivity(this)
 						.withToolbar(toolbar)
-						.withAccountHeader(headerResult)
+						.withAccountHeader(accountHeader)
 						.addDrawerItems(
-										new PrimaryDrawerItem().withName(R.string.drawer_item_own).withIcon(FontAwesome.Icon.faw_home),
-										new PrimaryDrawerItem().withName(R.string.drawer_item_favorited).withIcon(FontAwesome.Icon.faw_gamepad),
-										new DividerDrawerItem(),
-										new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog).withCheckable(false)
+										new PrimaryDrawerItem().withName(R.string.drawer_item_own).withIdentifier(ID_MY).withIcon(FontAwesome.Icon.faw_heart),
+										new PrimaryDrawerItem().withName(R.string.drawer_item_favorited).withIdentifier(ID_FAVORITE).withIcon(FontAwesome.Icon.faw_star)
 						) // add the items we want to use with our Drawer
-						.withOnDrawerItemClickListener((parent, view, position, id, drawerItem) -> {
-							if (drawerItem != null && drawerItem instanceof Nameable) {
-								getSupportActionBar().setTitle(((Nameable) drawerItem).getNameRes());
-							}
-						})
+						.withOnDrawerItemClickListener((parent, view, position, id, drawerItem) -> drawerItem != null && drawerItem instanceof Nameable && onClickDrawerItem(drawerItem))
 						.withOnDrawerListener(new Drawer.OnDrawerListener() {
 							@Override
 							public void onDrawerOpened(View drawerView) {
@@ -90,23 +96,42 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Main
 							@Override
 							public void onDrawerClosed(View drawerView) {
 							}
+							@Override
+							public void onDrawerSlide(View view, float v) {
+							}
 						})
+						.addStickyDrawerItems(
+										new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIdentifier(ID_SETTINGS).withIcon(FontAwesome.Icon.faw_cog).withCheckable(false)
+						)
 						.withFireOnInitialOnClick(true)
+						.withAnimateDrawerItems(true)
+						.withCloseOnClick(true)
 						.withSavedInstance(savedInstanceState)
 						.build();
 
 		//react on the keyboard
-		result.keyboardSupportEnabled(this, true);
+		drawer.keyboardSupportEnabled(this, true);
 
 		getFragmentManager().beginTransaction().replace(R.id.fragment_container, new TrackableListFragment()).commit();
+	}
+
+	private boolean onClickDrawerItem(IDrawerItem drawerItem) {
+		switch (drawerItem.getIdentifier()) {
+			case ID_SETTINGS:
+				// show settings
+				return true;
+			default:
+				setTitle(((Nameable)drawerItem).getNameRes());
+				return true;
+		}
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 
-		headerResult.clear();
-		headerResult.addProfiles(createProfile());
+		accountHeader.clear();
+		accountHeader.addProfiles(createProfile());
 
 		if (!accountService.isAccount()) {
 			startActivity(new Intent(this, WelcomeActivity.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
@@ -128,14 +153,14 @@ public class MainActivity extends AppCompatActivity implements HasComponent<Main
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
-		outState = result.saveInstanceState(outState);
-		outState = headerResult.saveInstanceState(outState);
+		outState = drawer.saveInstanceState(outState);
+		outState = accountHeader.saveInstanceState(outState);
 		super.onSaveInstanceState(outState);
 	}
 	@Override
 	public void onBackPressed() {
-		if (result != null && result.isDrawerOpen()) {
-			result.closeDrawer();
+		if (drawer != null && drawer.isDrawerOpen()) {
+			drawer.closeDrawer();
 		} else {
 			super.onBackPressed();
 		}
